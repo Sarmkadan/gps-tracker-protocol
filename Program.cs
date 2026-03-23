8e50814e316c18cfdcaed6504d94c08850087625
Vladyslav Zaiets <rutova2@gmail.com>
2026-03-23 20:40:17 +0300

    style: enable nullable, apply code quality improvements

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 // CTO & Software Architect
 // =============================================================================
 
+#nullable enable
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using GpsTrackerProtocol;
