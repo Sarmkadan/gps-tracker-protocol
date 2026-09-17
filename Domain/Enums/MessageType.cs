bfce84fc5bda17b64c26b469ec0cf7ceb9abb80e
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-17 18:49:57 +0000

    docs: add XML documentation comments to MessageType enum

--- Domain/Enums/MessageType.cs
+++ Domain/Enums/MessageType.cs
@@ -11,11 +11,18 @@ namespace GpsTrackerProtocol.Domain.Enums;
 /// </summary>
 public enum MessageType
 {
+    /// <summary>Unknown or unrecognized message type.</summary>
     Unknown = 0,
+    /// <summary>Acknowledgment message.</summary>
     Ack = 1,
+    /// <summary>Error message.</summary>
     Error = 2,
+    /// <summary>Location update message.</summary>
     LocationUpdate = 3,
+    /// <summary>Status message.</summary>
     Status = 4,
+    /// <summary>Alarm message.</summary>
     Alarm = 5,
+    /// <summary>Heartbeat message.</summary>
     Heartbeat = 6
 }
